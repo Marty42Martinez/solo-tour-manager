23edba5561a832e810cd63d97a85e08feb991d9e
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-22 11:05:44 -0700

    error middleware tested

--- test/middleware/error.test.js
+++ test/middleware/error.test.js
@@ -15,4 +15,18 @@ describe('error middleware', () => {
 
 
   });
+  it('sets error status with Error class and sends message', () => {
+    const error = new Error('Not found');
+    error.status = 404;
+    const res = {}
+    res.status = jest.fn(() => res);
+    res.send = jest.fn(() => res);
+
+    errorMiddleware(error, {}, res, () => { });
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledWith({
+      error: error.message
+    });
+  });
 });
